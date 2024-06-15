10557b84fb8c8934416e8af83e06093d538ffee2
Zala <zala.developer@gmail.com>
2024-06-15 14:59:23 +0930

    complete about and view all stones page

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('infinity_stones/', views.infinity_stones_index,
+         name="infinity_stones_index")
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+infinity_stones = [
+    {'name': 'Space Stone', 'color': 'blue',
+        'description': 'controls space and allows teleportation', 'owner': 'Thanos'},
+    {'name': 'Reality Stone', 'color': 'red',
+        'description': 'alters reality according to the wielder\'s will', 'owner': 'Malekith'},
+    {'name': 'Power Stone', 'color': 'purple',
+        'description': 'grants immense power and energy manipulation', 'owner': 'Star-Lord'},
+    {'name': 'Mind Stone', 'color': 'yellow',
+        'description': 'enhances mental abilities and grants access to all thoughts', 'owner': 'Vision'},
+    {'name': 'Time Stone', 'color': 'green',
+        'description': 'controls time, allowing time travel and manipulation', 'owner': 'Doctor Strange'},
+    {'name': 'Soul Stone', 'color': 'orange',
+        'description': 'controls souls and grants access to the soul world', 'owner': 'Red Skull'},
+]
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def infinity_stones_index(request):
+    return render(request, 'infinity_stones/index.html', {
+        'infinity_stones': infinity_stones
+    })
