d108c2e01287ac8b060e29204324aaa1fea4c640
Zala <zala.developer@gmail.com>
2024-06-15 15:30:48 +0930

    add InfinityStones model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class InfinityStones(models.Model):
+    name = models.CharField(max_length=15)
+    color = models.CharField(max_length=10)
+    description = models.TextField(max_length=250)
+    owner = models.CharField(max_length=50)
